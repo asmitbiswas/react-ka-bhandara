dd959d17cc8bb271e929da8188fcd93b8ba191a8
Asmit Biswas <asmitbiswas@gmail.com>
2026-03-17 21:57:55 +0530

    TOdays commit (-_-)

---
+++ app4/src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const app = function(){
+  const getFrontPageHtml = noteCount => {
+  return `
+    <!DOCTYPE html>
+    <html>
+      <head>
+      </head>
+      <body>
+        <div class='container'>
+          <h1>Full stack example app</h1>
+          <p>number of notes created ${noteCount}</p>
+          <a href='/notes'>notes</a>
+          <img src='kuva.png' width='200' />
+        </div>
+      </body>
+    </html>
+`
+}
+
+}
+
+
+
+app.get('/', (req, res) => {
+  const page = getFrontPageHtml(notes.length)
+  res.send(page)
+})
