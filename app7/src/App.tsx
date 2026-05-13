72b19cda53d8fa5b3d0d8d08f54459182ff05b21
asmitbiswas <asmitbiswas1308@gmail.com>
2026-05-13 11:22:21 +0530

    My maden component

---
+++ app7/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { Card } from "./MyComponentsAndHooks/card";
+
+function app(){
+  <Card
+  title="My card"
+  description="The img is so cool"
+  img="_.jpeg"
+
+  />
+}
+
+export default app
