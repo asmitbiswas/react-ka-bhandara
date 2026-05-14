8ad943c9e1d507f497e004881c3d83f4adb25fe6
asmitbiswas <asmitbiswas1308@gmail.com>
2026-05-14 19:21:39 +0530

    Minor, change in code ^-^^

--- app8/src/components/counter.tsx
+++ app8/src/components/counter.tsx
@@ -10,6 +10,8 @@ export function Counter(){
             <button onClick={() => {count + 1}}> Increment </button>
             <button onClick={() => {count - 1}}> Decrement </button>
             <button onClick={() => setCount(0)}> Reset </button>
+            <button onClick={() => {count + 100}}> Increment-1 </button>
+            
         </div>
     )
 }
